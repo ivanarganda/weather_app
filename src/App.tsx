7d173b85cec4d21a08b9d1c40a8433060397dd97
ivanarganda <ivanartista96@gmail.com>
2024-05-18 21:32:07 +0200

    correct bug about responsive mobile issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,56 @@
-import React, { FC, useContext, useEffect , lazy , Suspense } from 'react';
+import React, { FC, useContext, useEffect, lazy, Suspense } from 'react';
 import { GeolocalizationContext } from './context/weatherContext'; 
 
-const LeftSection = lazy(()=>import('./components/LeftSection'));
-const RightSection = lazy(()=>import('./components/RightSection'));
-const BottomSection = lazy(()=>import('./components/BottomSection'));
-const SpainMap = lazy(()=>import('./components/SpainMap'));
-
+const LeftSection = lazy(() => import('./components/LeftSection'));
+const RightSection = lazy(() => import('./components/RightSection'));
+const BottomSection = lazy(() => import('./components/BottomSection'));
+const SpainMap = lazy(() => import('./components/SpainMap'));
 
 const App: FC = () => {
-  const { condition  } = useContext(GeolocalizationContext);  
-  
+  const { condition } = useContext(GeolocalizationContext);  
+
   useEffect(() => {
     const setBackgroundImage = async () => {
-      if (condition!== undefined) {
+      if (condition !== undefined) {
         let imageWeather = condition?.weather.trim(); 
         try {   
-          
-          // Dynamically import the image
           const imageURL = await import(`./assets/wallpapersWeather/${condition?.forecast?.is_day === 1 ? 'morning' : 'night'}/${imageWeather}.jpg`); 
 
           // Set background image
-          $('.body').css('background-image', `url(${imageURL?.default})`)
-                   .css('background-size', 'cover')
-                   .css('background-repeat', 'no-repeat')
-                   .css('background-attachment', 'fixed');
+          const bodyElement = document.querySelector('.body') as HTMLElement;
+          if (bodyElement) {
+            bodyElement.style.backgroundImage = `url(${imageURL.default})`;
+            bodyElement.style.backgroundSize = 'cover';
+            bodyElement.style.backgroundRepeat = 'no-repeat';
+            bodyElement.style.backgroundAttachment = 'fixed';
+          }
         } catch (error) {
-          // Handle error when image cannot be loaded
           console.error("Error loading background image:", error);
         }
       }  
     };
-    setBackgroundImage() 
-  }, [ condition])         
+    setBackgroundImage(); 
+  }, [condition]);         
 
   return (
     <Suspense fallback={null}>
-      <div
-        className="body w-full min-w-[350px] min-h-screen pb-2 flex flex-col"
-      >
+      <div className="body w-full min-w-[350px] min-h-screen pb-2 flex flex-col bg-gray-800 text-gray-100">
         {/* Header with both left and right sections */}
-        <aside className="h-1/2 w-full h-auto pb-20 rounded-lg flex flex-row justify-around pt-20 bg-gray-800 bg-opacity-40 text-gray-100">
+        <aside className="flex flex-col md:flex-row justify-around items-center w-full md:h-1/2 pb-20 pt-20 bg-gray-800 bg-opacity-40 rounded-lg">
           <LeftSection />
           <RightSection />
         </aside>
         {/* Footer with bottom section */}
-        <footer className="h-1/2 pt-20 w-full h-auto flex flex-col items-center text-gray-100">
+        <footer className="flex flex-col items-center w-full h-auto pt-20">
           <BottomSection />
         </footer>
         {/* Spain map */}
-        <map id='weatherMap' className="m-auto mt-20 w-1/2 bg-gray-800 rounded-lg bg-opacity-30 relative">
+        <map id='weatherMap' className="m-auto mt-20 w-11/12 md:w-1/2 bg-gray-800 rounded-lg bg-opacity-30 relative">
           <SpainMap /> 
         </map>
       </div>
     </Suspense>
-  )
-}
- 
+  );
+};
+
 export default App;
--- src/components/SpainMap.tsx
+++ src/components/SpainMap.tsx
@@ -1,61 +1,71 @@
 import React, { FC, useContext, useEffect, useMemo, useState } from 'react';
-import imageMap from './../assets/wallpapersWeather/mapSpain.png'
+import imageMap from './../assets/wallpapersWeather/mapSpain.png';
 import { GeolocalizationContext } from './../context/weatherContext';
 import { icons } from '../assets/IconsWeather/icons';
 import provinces from './Provinces';
 
 const SpainMap: FC = () => {
-    const { changeLocation, getCurrentWeather } = useContext(GeolocalizationContext);
-    const [ weatherData, setWeatherData ] = useState<any>(null);
-    
-    const memorizedProvinces = useMemo( () => provinces , []);
+  const { changeLocation, getCurrentWeather } = useContext(GeolocalizationContext);
+  const [weatherData, setWeatherData] = useState<any>(null);
 
-    useEffect(() => {
-        // Fetch weather data for each province
-        const fetchWeatherData = async () => {
-            try {
-                const weatherPromises = memorizedProvinces.map(province => getCurrentWeather(province.location.lat, province.location.lng));
-                const weatherResults = await Promise.all(weatherPromises);
-                setWeatherData(weatherResults);
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        };
+  const memorizedProvinces = useMemo(() => provinces, []);
 
-        fetchWeatherData();
-        
-    }, [getCurrentWeather,memorizedProvinces]);   
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      try {
+        const weatherPromises = memorizedProvinces.map(province =>
+          getCurrentWeather(province.location.lat, province.location.lng)
+        );
+        const weatherResults = await Promise.all(weatherPromises);
+        setWeatherData(weatherResults);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    };
 
-    return (
-        <React.Fragment>
-            <img src={imageMap} className='relative'  width='100%' alt="map of spain" />
-            {weatherData && memorizedProvinces.map((province, index) => (
-                
-                <React.Fragment key={index}> 
-                    <img src={require(`./../assets/Provinces/${province.name}.png`)} style={{
-                        position: 'absolute',
-                        top: '' + province.position.top,
-                        left: '' + province.position.left,
-                        width: '' + province.size.width,
-                        height: '' + province.size.height,
-                        background: '',
-                        borderRadius: '50%',
-                        zIndex: -1
-                    }} alt={province.name} /> 
-                    <span onClick={() => changeLocation(province.location.lat, province.location.lng)} className='cursor-pointer hover:scale-150 w-10 transition-all' style={{
-                        position: 'absolute',
-                        top: '' + province.iconWeather.top,
-                        left: '' + province.iconWeather.left,
-                        width: '' + province.iconWeather.width,
-                        height: '' + province.iconWeather.height,
-                        borderRadius: '50%', 
-                        fontSize:'23px',
-                        zIndex: 1
-                    }} dangerouslySetInnerHTML={{__html:icons[weatherData[index]]}} ></span>
-                </React.Fragment>
-            ))}
-        </React.Fragment>
-    )
+    fetchWeatherData();
+  }, [getCurrentWeather, memorizedProvinces]);
+
+  return (
+    <React.Fragment>
+      <img src={imageMap} className="relative w-full" alt="map of spain" />
+      {weatherData &&
+        memorizedProvinces.map((province, index) => (
+          <React.Fragment key={index}>
+            <img
+              src={require(`./../assets/Provinces/${province.name}.png`).default}
+              style={{
+                position: 'absolute',
+                top: province.position.top,
+                left: province.position.left,
+                width: province.size.width,
+                height: province.size.height,
+                borderRadius: '50%',
+                zIndex: -1,
+              }}
+              alt={province.name}
+            />
+            <span
+              onClick={() =>
+                changeLocation(province.location.lat, province.location.lng)
+              }
+              className="cursor-pointer hover:scale-150 transition-transform"
+              style={{
+                position: 'absolute',
+                top: province.iconWeather.top,
+                left: province.iconWeather.left,
+                width: province.iconWeather.width,
+                height: province.iconWeather.height,
+                borderRadius: '50%',
+                fontSize: '23px',
+                zIndex: 1,
+              }}
+              dangerouslySetInnerHTML={{ __html: icons[weatherData[index]] }}
+            ></span>
+          </React.Fragment>
+        ))}
+    </React.Fragment>
+  );
 };
 
 export default React.memo(SpainMap);
